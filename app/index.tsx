af0b3f624e3440eb39036983537ba56478b13d66
Naveen-McDev <naveenmcdeveloper@gmail.com>
2025-05-03 16:12:35 +0530

    app initial setup

---
+++ app/index.tsx
@@ -0,0 +1,51 @@
+import { FlatList, SafeAreaView, Text, View } from "react-native";
+
+export default function Index() {
+  const totoData = [
+    {
+      id: 1,
+      title: "Todo 1",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: "Todo 3",
+      isDone: false,
+    },
+    {
+      id: 4,
+      title: "Todo 4",
+      isDone: false,
+    },
+    {
+      id: 5,
+      title: "Todo 5",
+      isDone: false,
+    },
+  ];
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <FlatList
+        data={totoData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+}
